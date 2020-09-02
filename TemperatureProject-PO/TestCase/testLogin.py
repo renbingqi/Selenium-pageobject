7b2e50c06dd699ddabd9f76b1332d9672b8047ae
binqi <905578725@qq.com>
2020-09-02 23:48:02 +0800

    使用ddt数据驱动方式测试登录

--- TemperatureProject-PO/TestCase/testLogin.py
+++ TemperatureProject-PO/TestCase/testLogin.py
@@ -10,17 +10,21 @@ from PageObject.LoginPage import LoginPage
 import unittest
 from Utils.driver.driver import Driver
 from ddt import ddt,data,unpack
-# @ddt
+
+
+
+@ddt
 class TestLogin(unittest.TestCase):
     def setUp(self):
-        self.driver=Driver().main()
+        self.driver=Driver().main()  #得到一个浏览器对象
         self.driver.maximize_window()
-        self.loginpage=LoginPage(self.driver)
+        self.loginpage=LoginPage(self.driver) #实例化一个LoginPage对象并把浏览器对象传过去
         self.loginpage.open_brower(url="http://localhost:3000/#/login")#打开登录页面
-    # @data()
-    # @unpack
-    def test_login(self):
-        self.loginpage.login(username="admin",password="admin")
+
+    @data(*[("admin","admin"),("vivalnk","vivalnk")])
+    @unpack
+    def test_login(self,username,password):
+        self.loginpage.login(username=username,password=password)
         self.assertEqual(self.driver.title,"Home")
 
     def tearDown(self):
